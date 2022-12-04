f2d8c8f404e4a7a516f557b9c0951ab3e1322182
Nalbert de Jesus <nalbert-edm@outlook.com>
2022-12-04 17:14:40 -0300

    Adicionado gitignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
 //TMDB
-const API_KEY = "api_key=9be6fa2e69ed6400adaddf24b022a10d";
-const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
-const searchURL = BASE_URL + "/search/movie?" + API_KEY;
+
 
 const genres = [
   {
